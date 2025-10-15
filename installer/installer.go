c9f0693eb3e157f582ea3e52a861b9fc9804e5a8
unknown321 <unknown321@users.noreply.github.com>
2025-10-15 20:15:04 +0300

    Revert "#76 fix repeated installation"
    This reverts commit 0db4feffb34bff4d1d380330ffb8f24dcd78b97d.

--- installer/installer.go
+++ installer/installer.go
@@ -327,25 +327,22 @@ func replaceFiles(outdir string) error {
 			}
 		}
 
-		toDelete := []string{}
-		copy(toDelete, f.Delete)
-
 		// TODO less loops?
 	Start:
 		for i, e := range sourceFpk.Entries {
-			for k, d := range toDelete {
+			for k, d := range f.Delete {
 				if e.FilePath.Data == d {
 					sourceFpk.Entries = slices.Delete(sourceFpk.Entries, i, i+1)
-					toDelete = slices.Delete(toDelete, k, k+1)
+					f.Delete = slices.Delete(f.Delete, k, k+1)
 					slog.Info("deleted", "name", d, "fpk", f.Source.NameInQar)
 					goto Start
 				}
 			}
 		}
 
-		if len(toDelete) != 0 {
+		if len(f.Delete) != 0 {
 			ff := ""
-			for _, v := range toDelete {
+			for _, v := range f.Delete {
 				ff += v + ","
 			}
 			ff = strings.TrimSuffix(ff, ",")
